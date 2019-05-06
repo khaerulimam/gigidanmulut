c75b2d706aa89b586ceb554214f8163e8e263cf7
irvandandung <irvandandung1@gmail.com>
2019-05-06 20:59:49 +0700

    update login

--- application/views/template_user/footer.php
+++ application/views/template_user/footer.php
@@ -11,44 +11,7 @@
         </div>
     </div>
     <!-- Footer End-->
-    <!-- Chat Box Start-->
-    <div class="chat-list-wrap">
-        <div class="chat-list-adminpro">
-            <div class="chat-button">
-                <span data-toggle="collapse" data-target="#chat" class="chat-icon-link"><i class="fa fa-comments"></i></span>
-            </div>
-            <div id="chat" class="collapse chat-box-wrap shadow-reset animated zoomInLeft">
-                <div class="chat-main-list">
-                    <div class="chat-heading">
-                        <h2>Messanger</h2>
-                    </div>
-                    <div class="chat-content chat-scrollbar">
-                        <div class="author-chat">
-                            <h3>Monica <span class="chat-date">10:15 am</span></h3>
-                            <p>Hi, what you are doing and where are you gay?</p>
-                        </div>
-                        <div class="client-chat">
-                            <h3>Mamun <span class="chat-date">10:10 am</span></h3>
-                            <p>Now working in graphic design with coding and you?</p>
-                        </div>
-                        <div class="author-chat">
-                            <h3>Monica <span class="chat-date">10:05 am</span></h3>
-                            <p>Practice in programming</p>
-                        </div>
-                        <div class="client-chat">
-                            <h3>Mamun <span class="chat-date">10:02 am</span></h3>
-                            <p>That's good man! carry on...</p>
-                        </div>
-                    </div>
-                    <div class="chat-send">
-                        <input type="text" placeholder="Type..." />
-                        <span><button type="submit">Send</button></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Chat Box End-->
+   
     <!-- jquery
 		============================================ -->
     <script src="http://localhost/gigidanmulut/assets/js/vendor/jquery-1.11.3.min.js"></script>
--- application/views/v_login.php
+++ application/views/v_login.php
@@ -53,6 +53,32 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
     <!-- Header top area start-->
+
+    <div class="wrapper-pro">
+       
+       <!-- Header top area start-->
+       <div  style="margin-left:none !important;">
+           <div class="header-top-area">
+               
+                   <div class="container-fluid">
+                       <div class="row">
+                         
+                           <div  class="col-lg-6 col-md-6 col-sm-6 col-xs-12"">
+                               <div class="header-top-menu tabl-d-n">
+                                   <ul class="nav navbar-nav mai-top-nav">
+                                       <li ><a href="<?php echo base_url(); ?>index.php/c_user" class="nav-link" >Home</a> </li>
+                                       <li ><a href="<?php echo base_url(); ?>index.php/c_user/halaman_konsultasi">Konsultasi</a></li>
+                                       <li ><a href="<?php echo base_url(); ?>index.php/C_akses" class="nav-link">Login Admin Pakar</a></li>
+                                       
+                                   </ul>
+                               </div>
+                           </div>
+                                                  
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <!-- Header top area end-->
     
 
             <!-- login Start-->
@@ -127,6 +153,20 @@
             <!-- login End-->
         </div>
     </div>
+
+    <!-- Footer Start-->
+<div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                        <p>Copyright &#169; 2018 Colorlib All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
     <!-- jquery
 		============================================ -->
         <script src="<?php echo base_url(); ?>assets/js/vendor/jquery-1.11.3.min.js"></script>
