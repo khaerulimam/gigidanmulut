e0472aab4d92e6335957140a6042873f7d280c5d
irvandandung <irvandandung1@gmail.com>
2019-05-20 08:25:06 +0700

    update db

--- gigidanmulut.sql
+++ gigidanmulut.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2019 at 04:47 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Waktu pembuatan: 20 Bulan Mei 2019 pada 03.22
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,17 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_admin`
+-- Struktur dari tabel `tb_admin`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_admin` (
+CREATE TABLE `tb_admin` (
   `id` int(200) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_admin`
+-- Dumping data untuk tabel `tb_admin`
 --
 
 INSERT INTO `tb_admin` (`id`, `username`, `password`) VALUES
@@ -42,10 +44,11 @@ INSERT INTO `tb_admin` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_gejala`
+-- Struktur dari tabel `tb_gejala`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_gejala` (
+CREATE TABLE `tb_gejala` (
+  `id` int(11) NOT NULL,
   `kd_gejala` varchar(15) NOT NULL,
   `nama_gejala` varchar(256) NOT NULL,
   `keterangan` text NOT NULL
@@ -54,20 +57,20 @@ CREATE TABLE IF NOT EXISTS `tb_gejala` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pasien`
+-- Struktur dari tabel `tb_pasien`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_pasien` (
+CREATE TABLE `tb_pasien` (
   `id_pasien` int(20) NOT NULL,
   `nama_pasien` varchar(50) NOT NULL,
   `umur` varchar(20) NOT NULL,
   `no_hp` varchar(20) NOT NULL,
   `jenis_kelamin` varchar(20) NOT NULL,
   `alamat` varchar(200) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_pasien`
+-- Dumping data untuk tabel `tb_pasien`
 --
 
 INSERT INTO `tb_pasien` (`id_pasien`, `nama_pasien`, `umur`, `no_hp`, `jenis_kelamin`, `alamat`) VALUES
@@ -77,10 +80,11 @@ INSERT INTO `tb_pasien` (`id_pasien`, `nama_pasien`, `umur`, `no_hp`, `jenis_kel
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_penyakit`
+-- Struktur dari tabel `tb_penyakit`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_penyakit` (
+CREATE TABLE `tb_penyakit` (
+  `id` int(11) NOT NULL,
   `kd_diagnosa` varchar(20) NOT NULL,
   `nama_diagnosa` varchar(256) NOT NULL,
   `keterangan` text NOT NULL,
@@ -90,19 +94,19 @@ CREATE TABLE IF NOT EXISTS `tb_penyakit` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_relasi`
+-- Struktur dari tabel `tb_relasi`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_relasi` (
+CREATE TABLE `tb_relasi` (
   `id` int(15) NOT NULL,
   `kd_diagnosa` varchar(20) NOT NULL,
   `kd_gejala` varchar(20) NOT NULL,
   `mb` double NOT NULL,
   `md` double NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_relasi`
+-- Dumping data untuk tabel `tb_relasi`
 --
 
 INSERT INTO `tb_relasi` (`id`, `kd_diagnosa`, `kd_gejala`, `mb`, `md`) VALUES
@@ -116,82 +120,93 @@ INSERT INTO `tb_relasi` (`id`, `kd_diagnosa`, `kd_gejala`, `mb`, `md`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_riwayat_pasien`
+-- Struktur dari tabel `tb_riwayat_pasien`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_riwayat_pasien` (
+CREATE TABLE `tb_riwayat_pasien` (
   `id` int(20) NOT NULL,
   `id_pasien` int(20) NOT NULL,
   `gejala` text NOT NULL,
   `penyakit` text NOT NULL,
   `CF` varchar(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_admin`
+-- Indeks untuk tabel `tb_admin`
 --
 ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_gejala`
+-- Indeks untuk tabel `tb_gejala`
 --
 ALTER TABLE `tb_gejala`
-  ADD PRIMARY KEY (`kd_gejala`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `kd_gejala` (`kd_gejala`);
 
 --
--- Indexes for table `tb_pasien`
+-- Indeks untuk tabel `tb_pasien`
 --
 ALTER TABLE `tb_pasien`
   ADD PRIMARY KEY (`id_pasien`);
 
 --
--- Indexes for table `tb_penyakit`
+-- Indeks untuk tabel `tb_penyakit`
 --
 ALTER TABLE `tb_penyakit`
-  ADD PRIMARY KEY (`kd_diagnosa`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_relasi`
+-- Indeks untuk tabel `tb_relasi`
 --
 ALTER TABLE `tb_relasi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_riwayat_pasien`
+-- Indeks untuk tabel `tb_riwayat_pasien`
 --
 ALTER TABLE `tb_riwayat_pasien`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_admin`
+-- AUTO_INCREMENT untuk tabel `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `tb_pasien`
+-- AUTO_INCREMENT untuk tabel `tb_pasien`
 --
 ALTER TABLE `tb_pasien`
-  MODIFY `id_pasien` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id_pasien` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
--- AUTO_INCREMENT for table `tb_relasi`
+-- AUTO_INCREMENT untuk tabel `tb_penyakit`
+--
+ALTER TABLE `tb_penyakit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_relasi`
 --
 ALTER TABLE `tb_relasi`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
--- AUTO_INCREMENT for table `tb_riwayat_pasien`
+-- AUTO_INCREMENT untuk tabel `tb_riwayat_pasien`
 --
 ALTER TABLE `tb_riwayat_pasien`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
